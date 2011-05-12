677fd906144d292c9580c95b09f8f41bb09ee514
Alain O'Dea <alain.odea@gmail.com>
2011-05-12 14:25:57 -0230

    Use iText 2.1.7 through a managed SBT dependency

--- project/build/LandscapePdfProject.scala
+++ project/build/LandscapePdfProject.scala
@@ -2,4 +2,6 @@ import sbt._
 
 class LandscapePdfProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val scalaTest = "org.scalatest" % "scalatest" % "1.3" withSources()
+  val jewelcli = "uk.co.flamingpenguin.jewelcli" % "jewelcli" % "0.6" withSources() withJavadoc()
+  val iText = "com.lowagie" % "itext" % "2.1.7" withSources() withJavadoc()
 }
